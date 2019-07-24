71f641d832a523af70823d5cd3a3ea5f5748943e
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-07-24 06:32:21 -0400

    Updated encryption to AES-CBC-HMAC
    Introduced message authentication to prevent padding oracle attack

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Core Functions
+ * Helper Functions Library
  */
 
-// Encrypt data
+// Encrypt data using AES-CBC-HMAC
 function rja_encrypt($plaintext)
 {
 
@@ -12,19 +12,27 @@ function rja_encrypt($plaintext)
 	$key = hash('sha256', PASS_CODE);
 	$iv = random_bytes(16);
 
-	$ciphertext = openssl_encrypt( $plaintext, $cipher, $key, 0, $iv );
-	return base64_encode($ciphertext . '::' . $iv);
+	$ciphertext = openssl_encrypt($plaintext, $cipher, $key, 0, $iv);
+	$hash = hash_hmac('sha256', $ciphertext, $key);
+
+	return base64_encode($ciphertext . '::' . $hash . '::' . $iv);
 
 }
 
-// Decrypt data
+// Decrypt data using AES-CBC-HMAC
 function rja_decrypt($encrypted)
 {
 
 	$cipher = 'aes-256-cbc';
 	$key = hash('sha256', PASS_CODE);
 
-	list($ciphertext, $iv) = explode('::', base64_decode($encrypted));
-	return openssl_decrypt($ciphertext, $cipher, $key, 0, $iv);
+	list($ciphertext, $hash, $iv) = explode( '::', base64_decode($encrypted) );
+	$calc_hash = hash_hmac('sha256', $ciphertext, $key);
+
+	if ( hash_equals($hash, $calc_hash) ) {
+		return openssl_decrypt($ciphertext, $cipher, $key, 0, $iv);
+	} else {
+		return 'Ciphertext has been compromised.';
+	}
 
 }
