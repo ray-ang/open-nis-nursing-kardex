e9407d333347d3a2658ea81cdeabfb3830c89268
ray-ang <46805160+ray-ang@users.noreply.github.com>
2019-07-25 07:12:18 -0400

    2 keys for encryption-hmac - $key and $key_hmac
    Derived two separate keys for encryption and HMAC from PASS_CODE constant

--- functions.php
+++ functions.php
@@ -10,10 +10,11 @@ function rja_encrypt($plaintext)
 
 	$cipher = 'aes-256-cbc';
 	$key = hash('sha256', PASS_CODE);
+	$key_hmac = hash( 'sha256', md5(PASS_CODE) );
 	$iv = random_bytes(16);
 
 	$ciphertext = openssl_encrypt($plaintext, $cipher, $key, 0, $iv);
-	$hash = hash_hmac('sha256', $ciphertext, $key);
+	$hash = hash_hmac('sha256', $ciphertext, $key_hmac);
 
 	return base64_encode($ciphertext . '::' . $hash . '::' . $iv);
 
@@ -25,11 +26,12 @@ function rja_decrypt($encrypted)
 
 	$cipher = 'aes-256-cbc';
 	$key = hash('sha256', PASS_CODE);
+	$key_hmac = hash( 'sha256', md5(PASS_CODE) );
 
 	list($ciphertext, $hash, $iv) = explode( '::', base64_decode($encrypted) );
-	$calc_hash = hash_hmac('sha256', $ciphertext, $key);
+	$digest = hash_hmac('sha256', $ciphertext, $key_hmac);
 
-	if ( hash_equals($hash, $calc_hash) ) {
+	if ( hash_equals($hash, $digest) ) {
 		return openssl_decrypt($ciphertext, $cipher, $key, 0, $iv);
 	} else {
 		return 'Ciphertext has been compromised.';
