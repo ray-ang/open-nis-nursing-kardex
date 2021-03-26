6ec4fe2916384be4e89c04484dc44c339bb57726
ray-ang <46805160+ray-ang@users.noreply.github.com>
2021-03-26 12:52:13 -0400

    Set 'show_in_rest' to false

--- room.php
+++ room.php
@@ -54,7 +54,7 @@ function rja_register_room_cpt()
 	    'has_archive'        => true,
 	    'hierarchical'       => true,
 	    'menu_position'      => null,
-	    'show_in_rest'       => true,
+	    'show_in_rest'       => false,
 	    'rest_base'          => 'rooms',
 	    'rest_controller_class' => 'WP_REST_Posts_Controller',
 	    'supports'           => array( 'title', /*'editor',*/ 'author', 'custom-fields' )
